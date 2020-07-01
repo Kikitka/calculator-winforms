610cf8a699971c8fd194359a8c0de2d93270eecd
Kikitka <nikitashvarckop@mail.ru>
2020-07-01 18:32:06 +0700

    add calculator

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Calc
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputLine = new System.Windows.Forms.Label();
+            this.Plus_but = new System.Windows.Forms.Button();
+            this.Minus_but = new System.Windows.Forms.Button();
+            this.Multiply_but = new System.Windows.Forms.Button();
+            this.Divide_but = new System.Windows.Forms.Button();
+            this.Equally_but = new System.Windows.Forms.Button();
+            this.Clear_line = new System.Windows.Forms.Button();
+            this.Delete_symbol = new System.Windows.Forms.Button();
+            this.LeftBracket = new System.Windows.Forms.Button();
+            this.RightBracket = new System.Windows.Forms.Button();
+            this.History_text = new System.Windows.Forms.Label();
+            this.CalcHistory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputLine
+            // 
+            this.InputLine.AutoSize = true;
+            this.InputLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputLine.Location = new System.Drawing.Point(65, 26);
+            this.InputLine.Name = "InputLine";
+            this.InputLine.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.InputLine.Size = new System.Drawing.Size(0, 20);
+            this.InputLine.TabIndex = 0;
+            this.InputLine.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Plus_but
+            // 
+            this.Plus_but.Location = new System.Drawing.Point(258, 48);
+            this.Plus_but.Name = "Plus_but";
+            this.Plus_but.Size = new System.Drawing.Size(54, 42);
+            this.Plus_but.TabIndex = 1;
+            this.Plus_but.Text = "+";
+            this.Plus_but.UseVisualStyleBackColor = true;
+            this.Plus_but.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // Minus_but
+            // 
+            this.Minus_but.Location = new System.Drawing.Point(258, 97);
+            this.Minus_but.Name = "Minus_but";
+            this.Minus_but.Size = new System.Drawing.Size(54, 42);
+            this.Minus_but.TabIndex = 2;
+            this.Minus_but.Text = "-";
+            this.Minus_but.UseVisualStyleBackColor = true;
+            this.Minus_but.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // Multiply_but
+            // 
+            this.Multiply_but.Location = new System.Drawing.Point(258, 144);
+            this.Multiply_but.Name = "Multiply_but";
+            this.Multiply_but.Size = new System.Drawing.Size(54, 42);
+            this.Multiply_but.TabIndex = 3;
+            this.Multiply_but.Text = "*";
+            this.Multiply_but.UseVisualStyleBackColor = true;
+            this.Multiply_but.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // Divide_but
+            // 
+            this.Divide_but.Location = new System.Drawing.Point(258, 192);
+            this.Divide_but.Name = "Divide_but";
+            this.Divide_but.Size = new System.Drawing.Size(54, 42);
+            this.Divide_but.TabIndex = 4;
+            this.Divide_but.Text = "/";
+            this.Divide_but.UseVisualStyleBackColor = true;
+            this.Divide_but.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // Equally_but
+            // 
+            this.Equally_but.Location = new System.Drawing.Point(318, 144);
+            this.Equally_but.Name = "Equally_but";
+            this.Equally_but.Size = new System.Drawing.Size(114, 90);
+            this.Equally_but.TabIndex = 5;
+            this.Equally_but.Text = "=";
+            this.Equally_but.UseVisualStyleBackColor = true;
+            this.Equally_but.Click += new System.EventHandler(this.Equally_but_Click);
+            // 
+            // Clear_line
+            // 
+            this.Clear_line.Location = new System.Drawing.Point(318, 48);
+            this.Clear_line.Name = "Clear_line";
+            this.Clear_line.Size = new System.Drawing.Size(54, 42);
+            this.Clear_line.TabIndex = 6;
+            this.Clear_line.Text = "C";
+            this.Clear_line.UseVisualStyleBackColor = true;
+            this.Clear_line.Click += new System.EventHandler(this.Clear_line_Click);
+            // 
+            // Delete_symbol
+            // 
+            this.Delete_symbol.Location = new System.Drawing.Point(378, 48);
+            this.Delete_symbol.Name = "Delete_symbol";
+            this.Delete_symbol.Size = new System.Drawing.Size(54, 42);
+            this.Delete_symbol.TabIndex = 7;
+            this.Delete_symbol.Text = "<-";
+            this.Delete_symbol.UseVisualStyleBackColor = true;
+            this.Delete_symbol.Click += new System.EventHandler(this.Delete_symbol_Click);
+            // 
+            // LeftBracket
+            // 
+            this.LeftBracket.Location = new System.Drawing.Point(318, 97);
+            this.LeftBracket.Name = "LeftBracket";
+            this.LeftBracket.Size = new System.Drawing.Size(54, 42);
+            this.LeftBracket.TabIndex = 8;
+            this.LeftBracket.Text = "(";
+            this.LeftBracket.UseVisualStyleBackColor = true;
+            this.LeftBracket.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // RightBracket
+            // 
+            this.RightBracket.Location = new System.Drawing.Point(378, 96);
+            this.RightBracket.Name = "RightBracket";
+            this.RightBracket.Size = new System.Drawing.Size(54, 42);
+            this.RightBracket.TabIndex = 9;
+            this.RightBracket.Text = ")";
+            this.RightBracket.UseVisualStyleBackColor = true;
+            this.RightBracket.Click += new System.EventHandler(this.ButtonOnClick);
+            // 
+            // History_text
+            // 
+            this.History_text.AutoSize = true;
+            this.History_text.Location = new System.Drawing.Point(509, 29);
+            this.History_text.Name = "History_text";
+            this.History_text.Size = new System.Drawing.Size(52, 17);
+            this.History_text.TabIndex = 10;
+            this.History_text.Text = "History";
+            // 
+            // CalcHistory
+            // 
+            this.CalcHistory.AutoSize = true;
+            this.CalcHistory.Location = new System.Drawing.Point(509, 48);
+            this.CalcHistory.Name = "CalcHistory";
+            this.CalcHistory.Size = new System.Drawing.Size(0, 17);
+            this.CalcHistory.TabIndex = 11;
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 384);
+            this.Controls.Add(this.CalcHistory);
+            this.Controls.Add(this.History_text);
+            this.Controls.Add(this.RightBracket);
+            this.Controls.Add(this.LeftBracket);
+            this.Controls.Add(this.Delete_symbol);
+            this.Controls.Add(this.Clear_line);
+            this.Controls.Add(this.Equally_but);
+            this.Controls.Add(this.Divide_but);
+            this.Controls.Add(this.Multiply_but);
+            this.Controls.Add(this.Minus_but);
+            this.Controls.Add(this.Plus_but);
+            this.Controls.Add(this.InputLine);
+            this.Name = "Calc";
+            this.Text = "Calc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InputLine;
+        private System.Windows.Forms.Button Plus_but;
+        private System.Windows.Forms.Button Minus_but;
+        private System.Windows.Forms.Button Multiply_but;
+        private System.Windows.Forms.Button Divide_but;
+        private System.Windows.Forms.Button Equally_but;
+        private System.Windows.Forms.Button Clear_line;
+        private System.Windows.Forms.Button Delete_symbol;
+        private System.Windows.Forms.Button LeftBracket;
+        private System.Windows.Forms.Button RightBracket;
+        private System.Windows.Forms.Label History_text;
+        private System.Windows.Forms.Label CalcHistory;
+    }
+}
+
---
+++ Calc/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddButtons();
+        }
+
+        private void AddButtons()
+        {
+            Button[] but = new Button[10];
+
+            var x = 40;
+            var y = 160;
+            var buttonWidth = 35;
+            var buttonHeight = 35;
+
+            var k = 0;
+
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    if(i == 0)
+                    {
+                        buttonWidth += 80;
+                    }
+
+                    but[i] = new Button();
+                    but[i].BackColor = Color.White;
+                    but[i].Text = (k++).ToString();
+                    but[i].Location = new Point(x, y);
+                    but[i].Size = new Size(buttonWidth, buttonHeight);
+                    but[i].Click += ButtonOnClick;
+                    this.Controls.Add(but[i]);
+
+                    if (i == 0)
+                    {
+                        buttonWidth = 35;
+                        break;
+                    }
+
+                    x += 40;
+                }
+                x = 40;
+                y -= 40;
+            }
+        }
+
+        private void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            if(InputLine.Text == "0")
+            {
+                InputLine.Text = "";
+            }
+
+            var but = (Button)sender;
+
+            Regex num = new Regex(@"\d$");
+
+            if (!num.IsMatch(but.Text))
+            {
+                if (num.IsMatch(InputLine.Text)|| InputLine.Text.EndsWith(")") || but.Text == "(")
+                {
+                    //Проверка правильности написания скобок
+                    if (but.Text == "(")
+                    {
+                        if (!InputLine.Text.EndsWith(")") && !num.IsMatch(InputLine.Text))
+                        {
+                            InputLine.Text += but.Text;
+                        }
+                    }
+                    else
+                    {
+                        InputLine.Text += but.Text;
+                    }
+                }
+            }
+            else
+            {
+                InputLine.Text += but.Text;
+            }
+        }
+
+        private void Clear_line_Click(object sender, EventArgs e)
+        {
+            InputLine.Text = "";
+        }
+
+        private void Delete_symbol_Click(object sender, EventArgs e)
+        {
+            if (InputLine.Text.Length > 0)
+            {
+                InputLine.Text = InputLine.Text.Remove(InputLine.Text.Length - 1);
+            }
+        }
+
+        private void Equally_but_Click(object sender, EventArgs e)
+        {
+            if (InputLine.Text.Length > 0)
+            {
+                InputLine.Text = Calculate(InputLine.Text).ToString();
+            }
+        }
+
+        //Перевод в обратную польскую запись
+        private List<string> Convert_to_RPN(string str)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            Regex num = new Regex(@"\d");
+
+            //var temp_str = "";
+
+            List<string> temp_str = new List<string>();
+
+            if (Check_brackets(str))
+            {
+                for (var i = 0; i < str.Length; i++)
+                {
+                    string ch = "";
+                    ch += str[i];
+
+                    //Запись числа, в случае если оно больше 1-го символа
+                    if (Char.IsDigit(str[i]) && i < str.Length - 1)
+                    {
+                        while (i + 1 < str.Length && Char.IsDigit(str[i + 1]))
+                        {
+                            ch += str[++i];
+                        }
+                    }
+
+                    //Запись числа в массив
+                    if (num.IsMatch(ch))
+                    {
+                        temp_str.Add(ch);
+                    }
+                    else
+                    {
+                        //Работа с символами
+                        if (stack.Count > 0)
+                        {
+                            if (ch == ")")
+                            {
+                                while (stack.Peek() != "(")
+                                {
+                                    temp_str.Add(stack.Pop());
+                                }
+                                stack.Pop();
+                            }
+                            else if(ch == "(")
+                            {
+                                stack.Push(ch);
+                            }
+                            else if (Check_priority(ch) <= Check_priority(stack.Peek()))
+                            {
+                                while (stack.Count > 0 && Check_priority(ch) <= Check_priority(stack.Peek()))
+                                {
+                                    temp_str.Add(stack.Pop());    
+                                }
+                                stack.Push(ch);
+                            }
+                            else
+                            {
+                                stack.Push(ch);
+                            }
+                        }
+                        else
+                        {
+                            stack.Push(ch);
+                        }
+                    }
+                }
+
+                //Запись оставшихся символов из стека в массив
+                if (stack.Count > 0)
+                {
+                    while (stack.Count != 0)
+                    {
+                        temp_str.Add(stack.Pop());
+                    }
+                }
+
+                return temp_str;
+            }
+            else
+            {
+                throw new Exception("Неверное к-во введённых скобок");
+            }
+        }
+
+        //Проверка приоритета символа
+        private int Check_priority(string ch)
+        {
+            var priority_ch = 0;
+            
+            switch(ch)
+            {
+                case "(": priority_ch = 0;
+                    break;
+                case "+":
+                case "-":
+                    priority_ch = 1;
+                    break;
+                case "*":
+                case "/":
+                    priority_ch = 2;
+                    break;
+            }
+
+            return priority_ch;
+        }
+
+        //Проверка на правильное к-во введённых символов
+        private bool Check_brackets(string str)
+        {
+            var count = 0;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                {
+                    count++;
+                }
+                else if(str[i] == ')')
+                {
+                    count--;
+                }
+            }
+
+            if(count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private double Calculate(string enter_str)
+        {
+            var str = Convert_to_RPN(enter_str);
+            double result = 0.0;
+
+            Regex num = new Regex(@"\d");
+
+            Stack<string> stack = new Stack<string>();
+            if (str.Count > 1)
+            {
+                for (var i = 0; i < str.Count; i++)
+                {
+                    if (num.IsMatch(str[i]))
+                    {
+                        stack.Push(str[i]);
+                    }
+                    else
+                    {
+                        var num1 = Convert.ToDouble(stack.Pop());
+                        var num2 = Convert.ToDouble(stack.Pop());
+
+                        switch (str[i])
+                        {
+                            case "+":
+                                result = num1 + num2;
+                                break;
+                            case "-":
+                                result = num2 - num1;
+                                break;
+                            case "*":
+                                result = num1 * num2;
+                                break;
+                            case "/":
+                                result = num2 / num1;
+                                break;
+                        }
+
+                        stack.Push(result.ToString());
+                    }
+                }
+            }
+
+            Write_history(enter_str, result.ToString());
+
+            return result;
+        }
+
+        private void Write_history(string str, string result)
+        {
+            CalcHistory.Text += str + " = " + result + "\n";
+
+            //Очистка истории
+            var s = CalcHistory.Text;
+            var n = s.Count(c => c == '\n');
+
+            if(n >= 10)
+            {
+                CalcHistory.Text = "";
+            }
+        }
+    }
+}
